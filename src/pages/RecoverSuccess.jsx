5c55bfe268c00b518e45829d7259dde6b003076a
FrancoLT930 <francolt930@hotmail.com>
2025-12-05 01:00:19 -0500

    Codigo Terminado. Quedan cosas por pulir pero estoy cansado jefe. Atte: Franco

--- src/pages/RecoverSuccess.jsx
+++ src/pages/RecoverSuccess.jsx
@@ -1,20 +1,44 @@
 import { useNavigate } from "react-router-dom";
+import { HeaderHome } from "../components/home/Header";
 
 import "../styles/RecoverSuccess.css";
 
 export const RecoverSuccess = () => {
   const navigate = useNavigate();
 
+  const handleContinue = () => {
+    // === FORZAMOS AL ADMIN ===
+    const adminData = {
+      name: "Juan",
+      username: "admin",
+      type: "Chasky Premium",
+      profit: "152.20",
+      record: 253,
+      totalToday: 32,
+      isAdmin: true,
+      phone: "987654321",
+      email: "juan@chaskys.com"
+    };
+
+    localStorage.setItem("chaskysUser", JSON.stringify(adminData));
+    navigate("/home");
+  };
+
   return (
     <>
+      <HeaderHome showBack={true} />
+
       <section className="recover-container success">
-        <div className="success-modal">
+        <div className="success-modal2">
           <div className="success-check">✓</div>
           <h3>¡Cuenta recuperada con éxito!</h3>
         </div>
-        <button className="btn-continue" onClick={() => navigate("/login")}>
-          Continuar
-        </button>
+
+        <div className="bottom-white-section">
+          <button className="btn-continue" onClick={handleContinue}>
+            Continuar
+          </button>
+        </div>
       </section>
     </>
   );
